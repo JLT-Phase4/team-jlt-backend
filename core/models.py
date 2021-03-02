f497ed78d56fbc1c531ed3d63bf5a0764547d719
Logan Robinson <loganrobinson315@gmail.com>
2021-03-01 20:03:36 -0500

    added team

--- core/models.py
+++ core/models.py
@@ -10,8 +10,6 @@ class User(AbstractUser):
   )
     user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES, default=2)
     avatar = models.CharField(max_length=500, null=True, blank=True)
-    
-    
 
     def __str__(self):
         return self.username
@@ -81,7 +79,7 @@ class Notification(models.Model):
 
 
 class Feed(models.Model):
-    name = models.CharField(max_length=100)
+    team = models.ForeignKey('Team', on_delete=models.CASCADE)
     
     def __str__(self):
-        return self.name
+        return self.team
